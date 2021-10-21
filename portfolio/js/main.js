9b6cb6972751cce42c51cb4b2c3d61400bcdae81
lineprang <eaalpl@students.eaaa.dk>
2021-10-22 00:59:59 +0200

    alt

--- portfolio/js/main.js
+++ portfolio/js/main.js
@@ -2,20 +2,22 @@
 
 console.log("Ready 🎉");
 
+
+
 // mobile navigation start //
-
-
 window.addEventListener("load", () => {
     document.body.classList.remove("preload");
   });
   
   document.addEventListener("DOMContentLoaded", () => {
     const nav = document.querySelector(".nav");
-  
+ 
+    //åben navigation
     document.querySelector("#btnNav").addEventListener("click", () => {
       nav.classList.add("nav--open");
     });
-  
+
+  //luk navigation
   document.querySelector(".nav__overlay").addEventListener("click", () => {
     nav.classList.remove("nav--open");
   });
@@ -23,3 +25,5 @@ window.addEventListener("load", () => {
   });
   
   // mobile navigation slut //
+
+
